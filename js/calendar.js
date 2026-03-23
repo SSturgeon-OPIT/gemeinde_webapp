2ef7f8f944f99c59aa04f87999ec62752077689a
Samuel <samuel.s@students.opit.com>
2026-03-23 11:13:59 +0100

    Quickly create simply logic to implement FullCalendar + copy/paste objects in churchEvents array that includes Sunday Church details until end of June (first half of June). Will now beging fixing syntax errors in code, then will see how it looks on website and will add in CSS code to make it look nice

---
+++ js/calendar.js
@@ -0,0 +1,214 @@
+/* create constant variable that will store array of yearly Gemeinde Church Events  */
+const churchEvents =
+[{
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-04-05T10:00",
+    end:   "2026-04-05T12:00",
+    extendedProps:
+    {
+        Empfang: "Cornel Motogna",
+        Präsidium: "Volker Imhof",
+        Sprecher: "Samuel Bogner"
+
+    }
+ }
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-04-12T10:00",
+    end:   "2026-04-12T12:00",
+    extendedProps:
+    {
+        Empfang: "Jonathan Ramsden",
+        Präsidium: "Marco Seitter",
+        Sprecher: "Hartmut Sieber"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-04-19T10:00",
+    end:   "2026-04-19T12:00",
+    extendedProps:
+    {
+        Empfang: "Emanuel Fischer",
+        Präsidium: "Martin Bogner",
+        Sprecher: "Tobias Wachauf"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-04-26T10:00",
+    end:   "2026-04-26T12:00",
+    extendedProps:
+    {
+        Empfang: "Florentin Støvhase",
+        Präsidium: "Micha Imhof",
+        Sprecher: "Michael Siller"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-05-03T10:00",
+    end:   "2026-05-03T12:00",
+    extendedProps:
+    {
+        Empfang: "Jochen Pokowietz",
+        Präsidium: "Johannes Bogner",
+        Sprecher: "Chris McQueen"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-05-10T10:00",
+    end:   "2026-05-10T12:00",
+    extendedProps:
+    {
+        Empfang: "Günter Seyfang",
+        Präsidium: "Volker Imhof",
+        Sprecher: "Marcus Bauer"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-05-17T10:00",
+    end:   "2026-05-175T12:00",
+    extendedProps:
+    {
+        Empfang: "Jochen Pokowietz",
+        Präsidium: "Cornel Motogna",
+        Sprecher: "Micha Imhof"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-05-24T10:00",
+    end:   "2026-05-24T12:00",
+    extendedProps:
+    {
+        Empfang: "Peter Siller",
+        Präsidium: "Tobias Wachauf",
+        Sprecher: "Emanuel Fischer"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-05-31T10:00",
+    end:   "2026-05-31T12:00",
+    extendedProps:
+    {
+        Empfang: "Tobias Wachauf",
+        Präsidium: "Johannes Bogner",
+        Sprecher: "Rüdiger Fischer"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-06-07T10:00",
+    end:   "2026-06-07T12:00",
+    extendedProps:
+    {
+        Empfang: "Micha Imhof",
+        Präsidium: "Emanuel Fischer",
+        Sprecher: "Gabor Urban"
+
+    }
+ }
+
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-06-14T10:00",
+    end:   "2026-06-14T12:00",
+    extendedProps:
+    {
+        Empfang: "Chris McQueen",
+        Präsidium: "Rüdiger Fischer",
+        Sprecher: "Cornel Motogna"
+
+    }
+ }
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-06-21T10:00",
+    end:   "2026-06-21T12:00",
+    extendedProps:
+    {
+        Empfang: "Peter Siller",
+        Präsidium: "Emanuel Fischer",
+        Sprecher: "MArco Seitter"
+
+    }
+ }
+ {
+    title: "Sonntag - Bibel Stunde", 
+    start: "2026-06-28T10:00",
+    end:   "2026-06-28T12:00",
+    extendedProps:
+    {
+        Empfang: "Johannes Bogner",
+        Präsidium: "Marcus Bauer",
+        Sprecher: "Chris McQueen"
+
+    }
+ }
+];
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    const calendarEl = new FullCalendar.Calendar(calendarEl,
+        {
+            initialView: 'dayGridMonth',
+            locale: 'de',
+            firstDay: 1,
+            height: 'auto',
+
+            headerToolbar:
+            {
+                left: 'prev, next today',
+                center: 'title',
+                right: 'dayGridMonth, timeGridWeek, listMonth'
+            },
+            events: churchEvents,
+
+            eventClick: function(info)
+            {
+                showEventDetails(info.event);
+            }
+        });
+        calendarEl.rend();
+});
+
+function showEventDetails(event)
+{
+    document.getElementById("event-modal").style.display = "block";
+
+    document.getElementById("modal-title").textContent = event.title;
+
+    document.getElementById("modal-time").textContent = event.start.toLocaleString() + " - " + (event.end ? event.end.toLocaleString() : " ");
+
+    const d = event.extendedProps;
+
+    document.getElementById("modal-description").innerHTML = 
+    `Sprecher: ${d.sprecher}<br>
+    Empfang: ${d.empfang}<br>
+    Präsidium: ${d.presider}`;
+}
+
+function closeModal() 
+{
+    document.getElementById("event-modal").style.display = "none";
+}
