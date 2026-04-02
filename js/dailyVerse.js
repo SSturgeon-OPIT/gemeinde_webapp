34584e0136f742094d7c4ced2cdb51f4382d43e3
Samuel <samuel.s@students.opit.com>
2026-04-02 11:23:06 +0200

    Add images to images array and verses to verses array. Will add more verses momentarily

---
+++ js/dailyVerse.js
@@ -0,0 +1,35 @@
+
+
+// Array of Bible verse
+const verses =
+[
+    "Der Herr is mein Hirte, mir wird nichts mangeln. - Psalm 23,1",
+    "Ich vermag alles dur den, der mich stark macht. - Philipper 4,13",
+    "Der Herr ist mein Licht und mein Heil. - Psalm 27,1",
+    "Seid stark und mutig! - Josua 1,9",
+    ""
+];
+
+// Array of Background images to pair with above verses
+const images =
+[
+    "images/dbv1.webp",
+    "images/dbv2.webp",
+    "images/dbv3.avif",
+    "images/dbv4.avif",
+    "images/dbv5.avif",
+    "images/dbv6.avif",
+    "images/dbv7.avif",
+    "images/dbv8.avif",
+    "images/dbv10.avif",
+    "images/dbv11.avif",
+    "images/dbv12.avif",
+    "images/dbv13.avif",
+    "images/dbv14.avif",
+    "images/dbv15.avif",
+    "images/dbv16.avif",
+    "images/dbv17.avif",
+    "images/dbv18.avif",
+    "images/dbv19.avif",
+    "images/dbv20.avif"
+]
